4a01b3acdc7af797b1a8d3680c9d0b07616aff6c
Anastasiia Smagliy <anastasiiasmagliy@gmail.com>
2026-09-10 01:33:06 +0300

    ++

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ function startGame() {
             <div class="card-inner">
 
                 <div class="card-front">
-                <img src="PNG-cards/card_back_red.png" alt="Card back">
+                <img src="../PNG-cards/card_back_red.png" alt="Card back">
                 </div>
 
                 <div class="card-back">
